35c2f3440d3e20a678f19bb81505fdb708343ff7
RileDev <71150306+RileDev@users.noreply.github.com>
2024-01-08 00:58:19 +0100

    JavaScript Simple Image Button Slider

---
+++ script.js
@@ -0,0 +1,34 @@
+const allImagesContainer = document.querySelector(".all_images")
+const images = document.querySelectorAll("img");
+const backBtn = document.querySelector(".btn-back")
+const nextBtn = document.querySelector(".btn-next")
+const allImgIndexes = images.length - 1;
+
+let imgIndex = 0;
+
+backBtn.addEventListener("click", () => {
+    if(imgIndex == 0) {
+        imgIndex = allImgIndexes;
+        changeImage()
+        return;
+    }
+    imgIndex--;
+    changeImage()
+})
+
+nextBtn.addEventListener("click", () => {
+    if(imgIndex >= allImgIndexes) {
+        imgIndex = 0;
+        changeImage()
+        return;
+    }
+    imgIndex++;
+    changeImage()
+})
+
+const changeImage = () => {
+    const img = document.querySelector(`img[value-index='${imgIndex}']`);
+    const imgWidth = img.getBoundingClientRect().width;
+    let value = imgWidth * imgIndex;
+    allImagesContainer.style.transform = "translateX(" + (-value) + "px)"
+}
